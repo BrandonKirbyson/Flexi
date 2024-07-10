e9139851880fcb32473d5695d94ca55d2b591120
TobinPalmer <tobinkpalmer@gmail.com>
2024-07-10 10:36:58 -0700

    Two methods of data loading

--- src/lib/components/flex/FlexClassList.svelte
+++ src/lib/components/flex/FlexClassList.svelte
@@ -1,27 +1,41 @@
 <script lang="ts">
+	import type { Flex } from '@/lib/types/Flex';
+	import { onMount } from 'svelte';
 	import { ENDPOINTS, fetchEndpoint } from '../../util/endpoints';
+	import FlexClassItem from './FlexClassItem.svelte';
+	import FlexClassSearch from './FlexClassSearch.svelte';
 
-	// let classTuple: [string, Flex][] = [];
+	let classTuple: [string, Flex][] = [];
 
-	async function getClasses() {
-		return Object.entries(await fetchEndpoint(ENDPOINTS.GET.Flex.GetClasses));
-	}
-	// onMount(async () => {});
+	// async function getClasses() {
+	// 	classTuple = Object.entries(await fetchEndpoint(ENDPOINTS.GET.Flex.GetClasses));
+	// }
+
+	let loaded = false;
+	onMount(async () => {
+		classTuple = Object.entries(await fetchEndpoint(ENDPOINTS.GET.Flex.GetClasses));
+		loaded = true;
+	});
 </script>
 
 <div class="wrapper">
-	<!-- {#if classTuple.length === 0}
-		<h1>Loading...</h1> -->
-	<!-- {:else} -->
+	{#if !loaded}
+		<p>LOADING</p>
+	{:else}
+		<FlexClassSearch bind:classes={classTuple} />
+		{#each classTuple as [_, flex]}
+			<FlexClassItem {flex} />
+		{/each}
+	{/if}
+
 	<!-- {#await getClasses()}
 		LOADING
-	{:then classTuple}
+	{:then _}
 		<FlexClassSearch bind:classes={classTuple} />
 		{#each classTuple as [_, flex]}
 			<FlexClassItem {flex} />
 		{/each}
 	{/await} -->
-	<!-- {/if} -->
 </div>
 
 <style lang="scss">
