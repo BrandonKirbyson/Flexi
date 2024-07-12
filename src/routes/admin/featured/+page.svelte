53665fb54696132aac851d811d10a037c161a730
TobinPalmer <tobinkpalmer@gmail.com>
2024-07-11 18:27:23 -0700

    more verbose form fields

--- src/routes/admin/featured/+page.svelte
+++ src/routes/admin/featured/+page.svelte
@@ -1,29 +1,52 @@
 <script lang="ts">
-	import type { FlexFormProps } from '@/lib/types/Flex';
+	// const formFields: FlexFormProps = {
+	// 	title: '',
+	// 	description: '',
+	// 	room: '',
+	// 	seats: 0,
+	// 	firstName: '',
+	// 	lastName: ''
+	// };
 
-	const formFields: FlexFormProps = {
-		title: '',
-		description: '',
-		room: '',
-		seats: 0,
-		firstName: '',
-		lastName: ''
-	};
-
-	$: {
-		formFields;
-		console.log(formFields);
-	}
+	let title = '';
+	let description = '';
+	let room = '';
+	let seats = 0;
+	let firstName = '';
+	let lastName = '';
 </script>
 
 <div>
 	<form method="POST">
-		{#each Object.keys(formFields) as key}
+		<label>
+			Title
+			<input required type="text" bind:value={title} />
+		</label>
+		<label>
+			Description
+			<textarea bind:value={description}></textarea>
+		</label>
+		<label>
+			Room
+			<input required type="text" bind:value={room} />
+		</label>
+		<label>
+			Seats
+			<input required type="number" bind:value={seats} />
+		</label>
+
+		<label>
+			Name
+			<input required type="text" bind:value={firstName} />
+			<input required type="text" bind:value={lastName} />
+		</label>
+
+		<!-- {#each Object.keys(formFields) as key}
 			<label>
 				{key}
 				<input name={key} />
 			</label>
-		{/each}
+		{/each} -->
 		<button>Log in</button>
 	</form>
 </div>
