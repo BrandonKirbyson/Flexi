1b0ae8dd18ceda2bd48ee16097f61b68e13ae2e8
TobinPalmer <tobinkpalmer@gmail.com>
2024-07-12 14:42:39 -0700

    Fixed addSchedules

--- src/routes/api/flex/addSchedule/+server.ts
+++ src/routes/api/flex/addSchedule/+server.ts
@@ -1,44 +1,37 @@
+import { flexAdminCollection, scheduleAdminCollection } from '@/lib/firebase/admin';
+import type { ClassSchedule, FlexScheduleDocument } from '@/lib/types/FlexSchedule';
 import { HttpStatusCode } from '@/lib/types/HttpStatus';
 import { apiPost } from '@/lib/util/api';
-import { ENDPOINTS, fetchEndpoint } from '@/lib/util/endpoints';
+import { DAY_FORMAT } from '@/lib/util/date';
+import { ENDPOINTS } from '@/lib/util/endpoints';
 import type { RequestEvent } from '@sveltejs/kit';
 import dayjs from 'dayjs';
 
 export async function POST(event: RequestEvent) {
-	return await apiPost<typeof ENDPOINTS.POST.Flex.AddSchedule>(event, async (params) => {
+	return await apiPost<typeof ENDPOINTS.POST.Flex.AddFeaturedFlex>(event, async (params) => {
+		await Promise.resolve();
+		console.log('Req', params);
+
+		const classesDoc = await flexAdminCollection.doc('classes').get();
+		const data = classesDoc.data();
+		if (!classesDoc.exists || !data) return [null, HttpStatusCode.NOT_FOUND];
+		const classes = data.classes;
 		const date = dayjs(params.date);
 
-		const classes = await fetchEndpoint(ENDPOINTS.GET.Flex.GetClasses, {}, event.fetch);
-		// const data = classesDoc.data();
-		// if (!classesDoc.exists || !data) return [null, HttpStatusCode.NOT_FOUND];
-		// const classes = data.classes;
+		const doc: FlexScheduleDocument = {
+			date: date.format(DAY_FORMAT),
+			classes: Object.fromEntries(
+				Object.keys(classes).map((key) => [key, { students: [] as string[] } as ClassSchedule])
+			) as Record<string, ClassSchedule>
+		};
 
-		// const doc: FlexScheduleDocument = {
-		// 	date: date.format(DAY_FORMAT),
-		// 	classes: Object.fromEntries(
-		// 		Object.keys(classes).map((key) => [key, { students: [] as string[] } as ClassSchedule])
-		// 	) as Record<string, ClassSchedule>
-		// };
-		// const doc: FlexScheduleDocument = {
-		// 	type: FlexType.Featured,
-		// 	title: 'title',
-		// 	dept: FlexDept.Math,
-		// 	// room: course.courseRoom,
-		// 	room: 'room',
-		// 	teacher: {
-		// 		first: 'first',
-		// 		last: 'last'
-		// 	},
-		// 	seats: 30
-		// };
+		await scheduleAdminCollection.add(doc);
 
-		// await scheduleAdminCollection.add(doc);
+		const schedule = {
+			classes: doc.classes,
+			date: date
+		};
 
-		// const schedule = {
-		// 	classes: doc.classes,
-		// 	date: date
-		// };
-
-		return [null, HttpStatusCode.SUCCESS_CREATED];
+		return [null, HttpStatusCode.SUCCESS];
 	});
 }
