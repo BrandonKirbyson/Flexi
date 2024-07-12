64f8438dfcd461686b8020c8d99357c3b7f791c9
TobinPalmer <tobinkpalmer@gmail.com>
2024-07-12 11:47:54 -0700

    Uploading with UUID working

--- src/routes/api/uploadImage/+server.ts
+++ src/routes/api/uploadImage/+server.ts
@@ -3,62 +3,21 @@ import { HttpStatusCode } from '@/lib/types/HttpStatus';
 import { apiPost } from '@/lib/util/api';
 import { ENDPOINTS } from '@/lib/util/endpoints';
 import type { RequestEvent } from '@sveltejs/kit';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function POST(event: RequestEvent) {
 	return await apiPost<typeof ENDPOINTS.POST.UploadImage>(event, async (params) => {
-		// const storageRef = ref(storage, 'some-child');
-
 		try {
 			const buffer = Buffer.from(JSON.stringify(params.bytes));
-			console.log('buffer', buffer);
 
-			const fileName = 'jfoiadjsof';
-			const filePath = fileName;
-
+			const filePath = uuidv4();
 			const file = adminStorage.bucket().file(filePath);
 
-			console.log('file', file);
 			await file.save(buffer, { contentType: 'application/octet-stream' });
-			console.log(`File ${filePath} uploaded successfully.`);
+			return [file.publicUrl(), HttpStatusCode.SUCCESS_CREATED];
 		} catch (error) {
 			console.error('Error uploading file:', error);
+			return [null, HttpStatusCode.BAD_REQUEST];
 		}
-
-		// try {
-		// 	// Upload the blob
-		// 	adminStorage;
-		// 	console.log('Uploaded a blob or file!', snapshot.ref.fullPath);
-		// 	return [snapshot.ref.fullPath, HttpStatusCode.SUCCESS_CREATED];
-		// } catch (e) {
-		// 	console.error('Error uploading file', e);
-		// 	return [null, HttpStatusCode.BAD_REQUEST];
-		// }
-
-		// const date = dayjs(params.file);
-
-		// const schedules = await scheduleAdminCollection
-		// 	.where('date', '==', date.format(DAY_FORMAT))
-		// 	.limit(1)
-		// 	.get();
-
-		// if (schedules.empty) {
-		// 	return [null, HttpStatusCode.NOT_FOUND];
-		// }
-
-		// const schedule = schedules.docs[0].data();
-
-		// for (const key in schedule.classes) {
-		// 	if (schedule.classes[key].students.includes(params.studentId)) {
-		// 		schedule.classes[key].students = schedule.classes[key].students.filter(
-		// 			(student) => student !== params.studentId
-		// 		);
-		// 	}
-		// }
-
-		// schedule.classes[params.flexId].students.push(params.studentId);
-
-		// scheduleAdminCollection.doc(schedules.docs[0].id).set(schedule);
-
-		return [null, HttpStatusCode.SUCCESS_CREATED];
 	});
 }
