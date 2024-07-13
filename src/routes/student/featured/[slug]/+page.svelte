e34b5387eb7b977d9e2582c9427e8b32509946e8
TobinPalmer <tobinkpalmer@gmail.com>
2024-07-12 20:21:23 -0700

    added featured route

---
+++ src/routes/student/featured/[slug]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import FlexClassList from '@/lib/components/flex/FlexClassList.svelte';
+
+	export let data../../flex copy/[slug]/$types.js;
+</script>
+
+<FlexClassList date={data.date} />
---
+++ src/routes/student/upcoming/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	// fetchEndpoint(ENDPOINTS.GET.Flex);
+</script>
+
+<div></div>
+
+<style lang="scss">
+</style>
